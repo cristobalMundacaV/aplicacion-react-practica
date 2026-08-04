7db08d446055403be131fc5c7757ef4395a809e1
Cristobal Mundaca <cristobal.mundacav@gmail.com>
2026-08-04 11:35:40 -0400

    primera pagina construida 100% a mano

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className='app'>
+      <section className='card'>
+        <span className='badge'>Precio exclusivo</span>
+
+        <h1>Arroz con leche</h1>
+
+        <p>Aqui se construira la primera vista a mano de la historia wn</p>
+
+        <button type='button'>
+          Guardar precio
+        </button>
+      </section>
+    </main>
+  )
+}
+
+export default App
